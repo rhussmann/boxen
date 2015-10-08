d09d9321d57aaff234084c465057047bb9959c1b
Ricky Hussmann <ricky.hussmann@gmail.com>
2015-10-08 15:28:27 -0400

    Removing hard-coded user-specific configuration

--- modules/listhub/manifests/repos.pp
+++ modules/listhub/manifests/repos.pp
@@ -1,22 +1,22 @@
 class listhub::repos {
   repository {
     'listhub':
-      source => 'https://rhussmann@bitbucket.org/listhub-ondemand/listhub.git',
-      path => '/Users/rhussmann/Code/listhub',
+      source => 'https://bitbucket.org/listhub-ondemand/listhub.git',
+      path => "/Users/${::boxen_user}/Code/listhub",
       provider => 'git'
   }
 
   repository {
     'lh_tools':
-      source => 'https://rhussmann@bitbucket.org/listhub-ondemand/lh_tools.git',
-      path => '/Users/rhussmann/Code/lh_tools',
+      source => 'https://bitbucket.org/listhub-ondemand/lh_tools.git',
+      path => "/Users/${::boxen_user}/Code/lh_tools",
       provider => 'git'
   }
 
   repository {
     'infrastructure':
-      source => 'https://rhussmann@bitbucket.org/listhub-ondemand/infrastructure.git',
-      path => '/Users/rhussmann/Code/infrastructure',
+      source => 'https://bitbucket.org/listhub-ondemand/infrastructure.git',
+      path => "/Users/${::boxen_user}/Code/infrastructure",
       provider => 'git'
   }
 }
